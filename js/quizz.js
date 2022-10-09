f3d5100a955ddac4b3197da12f4121ac609bbb00
Stick60 <jonathan.verite@gmail.com>
2022-10-09 12:56:19 +0200

    quizz 2.0

--- js/quizz.js
+++ js/quizz.js
@@ -6,14 +6,38 @@ console.log(id);
 fetch("https://qcm.alwayslearn.fr/api/examens/" + id)
 .then((res)=> {
     res.json().then(elements=>{
-        elements['question'].forEach(element =>{
+        elements['question'].forEach(element => {
         getElement(element);
-        console.log(element)
+        let title = document.getElementById('titre')
+        title.innerHTML = elements.title;
+        console.log(element);
+        console.log(elements);
         });
     });
   
 });
 
+function getElement(element){    
+    let div1 = document.createElement('div');
+    div1.classList.add('card', 'mb-2');
+  
+    let div2 = document.createElement('div');
+    div2.classList.add('card-header');
+    div1.appendChild(div2);
+    div2.innerHTML = element.question;
+  
+    let div3 = document.createElement('div');
+    div3.classList.add('card-body');
+    div1.appendChild(div3);
+  
+    let radio1 = document.createElement('input');
+    radio1.type= 'radio';
+    radio1.name= '1';
+    let label1 = document.createElement('label');
+    label1.for = '1';
+    label1.innerHTML =element.options.option;
 
-
-
+    div3.appendChild(radio1);   
+     div3.appendChild(label1);
+    app.appendChild(div1);
+  }
--- js/quizz2.js
+++ js/quizz2.js
@@ -1,13 +1,32 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id')
-console.log(id);
-
-
-fetch("https://qcm.alwayslearn.fr/api/examens/" + id)
+let app = document.getElementById('app');
+fetch("https://qcm.alwayslearn.fr/api/examens")
 .then((res)=> {
-    res.json().then(element=>{
-      console.log(element);
+    res.json().then(elements=>{
+      elements['hydra:member'].forEach(element => {
+        getElement(element);
+      });
     });
   
 });
+
+function getElement(element){
+  let div1 = document.createElement('div');
+  div1.classList.add('card', 'mb-2');
+
+  let div2 = document.createElement('div');
+  div2.classList.add('card-header');
+  div1.appendChild(div2);
+  div2.innerHTML = element.title;
+
+  let div3 = document.createElement('div');
+  div3.classList.add('card-body');
+  div1.appendChild(div3);
+
+  let a = document.createElement('a');
+  a.href="quizz.html?id="+element.id;
+  a.classList.add('btn', 'btn-primary');
+  
+  a.innerHTML ="Démarrer l'examen";
+  div3.appendChild(a);
+  app.appendChild(div1);
+}
